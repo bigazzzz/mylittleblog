1b23887c885dcd94dfe3ecf68f13abc399a8645c
root <root@39711.simplecloud.club>
2016-12-27 00:21:00 +0300

    Technical Update

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$data['SITE_NAME'] = '<';
+$data['SITE_NAME'] = '';
 $data['db'] = array (
   'host' => '127.0.0.1',
-  'user' => 'root',
-  'pass' => '',
-  'dbname' => 'root',
+  'user' => 'blog',
+  'pass' => 'af26BTSQMADFB6ms',
+  'dbname' => 'blog',
 );
 $data['dir'] = '';

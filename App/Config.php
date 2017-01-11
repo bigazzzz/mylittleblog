d20cc06199bb9ababf58897d2bdcf6cf144c560d
bigazzzz <bigazzzz@gmail.com>
2017-01-11 03:33:54 +0300

    Убран дефолтный title

--- App/Config.php
+++ App/Config.php
@@ -5,20 +5,29 @@ class Config
 {
 
     use Singleton;
-    use GetterSetter;
+
     private $config_file;
 
     public function __construct($config_file = 'config')
     {
         $this->config_file = $config_file . '.php';
         include_once $this->config_file;
-        $this->data = $data;
+        $this->data = new ConfigItem($data);
+    }
+
+    public function __get($k)
+    {
+        if (is_array($this->data->$k)){
+            return new ConfigItem($this->data->$k);
+        } else {
+            return $this->data->$k;
+        }
     }
 
     public function save()
     {
         $config_string = "<?php\r\n";
-        foreach ($this->data as $k=>$v){
+        foreach ($this->data->data as $k=>$v){
             $config_string .= "\$data['" . $k . "'] = " . var_export($v, TRUE) . ";\r\n";
         }
         return file_put_contents($this->config_file, $config_string);
---
+++ App/ConfigItem.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: biga
+ * Date: 11.01.17
+ * Time: 1:35
+ */
+
+namespace App;
+
+
+class ConfigItem
+{
+
+}

793e25615e8492901c3300af26c0d7a468c1616c
bigazzzz <bigazzzz@gmail.com>
2017-02-28 23:17:52 +0300

    test

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+return array (
+  'site' =>
+  array (
+    'title' => 'Тестовый бложик',
+    'name' => 'ТЕСТ',
+    'copyright' => 'biga',
+    'publish_year' => '2016',
+    'meta' => 
+    array (
+      'name' => 
+      array (
+        'keywords' => 'блог',
+        'author' => 'biga',
+        'description' => 'Описание сайта',
+      ),
+    ),
+  ),
+  'db' =>
+  array (
+    'host' => '127.0.0.1',
+    'user' => 'blog',
+    'password' => '12345677',
+    'dbname' => 'blog',
+  ),
+  'twig' =>
+        array (
+            'template_dir' => '/var/www/blog/App/Views/Twig/default',
+            'cache_dir' => '/var/www/blog/Cache/Twig',
+        ),
+  'dir' => '',
+);

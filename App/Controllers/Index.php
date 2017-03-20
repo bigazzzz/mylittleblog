fbde45e15cbb4d7c5f60f24019f817012b186113
bigazzzz <bigazzzz@gmail.com>
2017-03-21 02:57:12 +0300

    Templates path refactoring

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -12,12 +12,13 @@ class Index extends Controller
     protected function beforeAction()
     {
         $this->view->site = Config::instance()->site;
-        $this->view->addTemplateDir(ROOT_DIR . '/Modules/Views/Blog/' . Config::instance()->template_engine);
+        $this->view->addTemplateDir(ROOT_DIR . '/Modules/Views/' . Config::instance()->template_engine);
     }
+
     protected function actionIndex()
     {
         $this->view->posts = \Modules\Models\Blog\Post::getLatest(Config::instance()->posts->count_on_start_page);
-        $this->view->content = $this->view->render('posts');
+        $this->view->content = $this->view->render('Blog/posts');
         $this->view->display('index');
     }
 
@@ -28,14 +29,14 @@ class Index extends Controller
     protected function actionPost()
     {
         $this->view->post = \Modules\Models\Blog\Post::findById($_REQUEST['id']);
-        $this->view->content = $this->view->render('post');
+        $this->view->content = $this->view->render('Blog/post');
         $this->view->display('index');
     }
 
     protected function actionAuthor()
     {
         $this->view->author = \Modules\Models\Blog\Author::findById($_REQUEST['id']);
-        $this->view->content = $this->view->render('author');
+        $this->view->content = $this->view->render('Blog/author');
         $this->view->display('index');
     }
 

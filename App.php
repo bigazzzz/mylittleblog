61f7ffcf2b105f06eefb6a2d953566c00ca8848e
socnetspam@gmail.com <bigazzzz@gmail.com>
2017-03-24 15:29:26 +0300

    Rename Router to Route. Refactoring

--- App.php
+++ App.php
@@ -10,13 +10,13 @@ class App
 	public function run()
 	{
 		try {
-			$this->route = \App\Router::get($_SERVER['REQUEST_URI']);
+			$this->route = \App\Route::get($_SERVER['REQUEST_URI']);
 		} catch (\App\Exceptions\RouteException $e){
 			echo "Ошибка роутинга - " . $e->getMessage();
 		}
-
-		$controller = new $this->route->controller;
-		$controller->action($this->route->action);
+		var_dump($this->route);
+//		$controller = new $this->route->controller;
+//		$controller->action($this->route->action);
 
 	}	
 

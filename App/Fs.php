a8decb907292a63c98f3b71aa7a6ff140b0aa1d6
socnetspam@gmail.com <bigazzzz@gmail.com>
2018-06-22 13:59:56 +0300

    refactoring

--- App/Fs.php
+++ App/Fs.php
@@ -1,18 +1,15 @@
 <?php
 namespace App;
 
-
 class Fs
 {
-public static findFiles($mask = "*.*")
-{
-return glob($mask, GLOB_BRACE);
-}
-
-public
-static findDir($mask = "*.*")
-{
-        return glob($mask, GLOB_BRACE | GLOB_ONLYDIR);
+    public static findFiles($mask = "*.*")
+    {
+        return glob($mask, GLOB_BRACE);
     }
 
+    public static findDir($mask = "*.*")
+    {
+            return glob($mask, GLOB_BRACE | GLOB_ONLYDIR);
+    }
 }
